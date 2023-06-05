bafbda1391efc8ba86c784bc30a4858f2c1d59b9
K3shav83 <135092088+K3shav83@users.noreply.github.com>
2023-06-05 19:52:11 +0530

    Update filters.py

--- SankiMusic/utilities/events/filters.py
+++ SankiMusic/utilities/events/filters.py
@@ -5,10 +5,10 @@ from pyrogram import filters
 from SankiMusic.utilities.config import COMMAND_PREFIXES
 
 
-# ╔══╗╔══╗╔═╦╗╔╦╗╔══╗  ╔═╦═╗╔╦╗╔══╗╔══╗╔═╗
-# ║══╣║╔╗║║║║║║╔╝╚║║╝  ║║║║║║║║║══╣╚║║╝║╔╝
-# ╠══║║╠╣║║║║║║╚╗╔║║╗  ║║║║║║║║╠══║╔║║╗║╚╗
-# ╚══╝╚╝╚╝╚╩═╝╚╩╝╚══╝  ╚╩═╩╝╚═╝╚══╝╚══╝╚═╝
+#   ╔═╦═╗╔╦╗╔══╗╔══╗╔═╗
+#   ║║║║║║║║║══╣╚║║╝║╔╝
+#   ║║║║║║║║╠══║╔║║╗║╚╗
+#   ╚╩═╩╝╚═╝╚══╝╚══╝╚═╝
 
 def command(commands: Union[str, List[str]]):
     return filters.command(commands, COMMAND_PREFIXES)
